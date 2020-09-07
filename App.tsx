94bc468e927c5a47476d295445db9fa66513f94b
Rafiou Sitou <rafiousitou90@gmail.com>
2020-09-07 20:57:55 -0300

    feat(): Add the app fonts and LoadAssets component.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const fonts = {
+	"Archivo-Bold": require("./assets/fonts/Archivo-Bold.ttf"),
+	"Archivo-Medium": require("./assets/fonts/Archivo-Medium.ttf"),
+	"Archivo-Regular": require("./assets/fonts/Archivo-Regular.ttf"),
+	"Archivo-SemiBold": require("./assets/fonts/Archivo-SemiBold.ttf"),
+	"Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+	"Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+	"Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+	"Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+})
